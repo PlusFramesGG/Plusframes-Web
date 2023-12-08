e2ba78e978398084befac894048625a69c602280
Charlie Sparks <sparksfullstack@gmail.com>
2023-12-08 13:17:17 -0800

    Charlie | Moved FB creds into constants

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,23 +1,28 @@
-// Import the functions you need from the SDKs you need
 import { getApps, initializeApp } from 'firebase/app'
 import { getAnalytics } from 'firebase/analytics'
-// TODO: Add SDKs for Firebase products that you want to use
+import {
+	FIREBASE_API_KEY,
+	FIREBASE_APP_ID,
+	FIREBASE_AUTH_DOMAIN,
+	FIREBASE_MEASUREMENT_ID,
+	FIREBASE_MESSAGING_SENDER_ID,
+	FIREBASE_PROJECT_ID,
+	FIREBASE_STORAGE_BUCKET
+} from '@/shared/constants'
+// TODO: We can add the SDKs for any additional firebase products below
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-	apiKey: 'AIzaSyAnNBSTdqF29DNtVbHyccSB-ih6rZs7ouw',
-	authDomain: 'plusframes-dev.firebaseapp.com',
-	projectId: 'plusframes-dev',
-	storageBucket: 'plusframes-dev.appspot.com',
-	messagingSenderId: '916275048567',
-	appId: '1:916275048567:web:9027825e341fb7773c9672',
-	measurementId: 'G-R9G0E3J7CH'
+	apiKey: FIREBASE_API_KEY,
+	authDomain: FIREBASE_AUTH_DOMAIN,
+	projectId: FIREBASE_PROJECT_ID,
+	storageBucket: FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+	appId: FIREBASE_APP_ID,
+	measurementId: FIREBASE_MEASUREMENT_ID
 }
 
-// Initialize Firebase
 const firebase_app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0]
-const analytics = getAnalytics(firebase_app)
+export const analytics = getAnalytics(firebase_app)
 
 export default firebase_app
--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,2 +1,7 @@
-// SETUP TODO: Add creds for backend services here
-// export const MONGODB_URI: string = process.env.MONGODB_URI!
+export const FIREBASE_API_KEY = process.env.FIREBASE_API_KEY!
+export const FIREBASE_AUTH_DOMAIN = process.env.FIREBASE_AUTH_DOMAIN!
+export const FIREBASE_PROJECT_ID = process.env.FIREBASE_PROJECT_ID!
+export const FIREBASE_STORAGE_BUCKET = process.env.FIREBASE_STORAGE_BUCKET!
+export const FIREBASE_MESSAGING_SENDER_ID = process.env.FIREBASE_MESSAGING_SENDER_ID!
+export const FIREBASE_APP_ID = process.env.FIREBASE_APP_ID!
+export const FIREBASE_MEASUREMENT_ID = process.env.FIREBASE_MEASUREMENT_ID!
